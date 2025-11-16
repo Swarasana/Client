dbb36e723ed210f535b5e67da31c5689c270b800
mikeleo03 <leonmichael463@gmail.com>
2025-11-16 23:29:06 +0700

    [Feat] Add PWA functionalities and framer motion

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,43 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: [
+        "**/*",
+      ],
+      manifest: {
+        "name": "Swarasana",
+        "short_name": "Swarasana",
+        "description": "Platform Komunal Inklusif Berbasis AI untuk Ruang Budaya",
+        "theme_color": "#FFFFFF",
+        "background_color": "#054FB9",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
+        "icons": [
+          {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      },
+    }),
+  ],
   server: {
     host: true,
     strictPort: true,
